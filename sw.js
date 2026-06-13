17a06bf63b27d82b0ee566655d99198c4139a775
palashmahmod <palashmahmod316@gmail.com>
2026-06-13 23:51:42 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meherin-diet-offline-v3';
+const CACHE_NAME = 'meherin-diet-offline-v4';
 const assets = [
   './',
   './index.html',
@@ -6,10 +6,27 @@ const assets = [
   './photo2.jpg'
 ];
 
+// ১. নতুন ফাইল আসার সাথে সাথে পুরোনো সার্ভিস ওয়ার্কারকে জোর করে সরিয়ে দেবে
 self.addEventListener('install', e => {
+  self.skipWaiting(); 
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
+    })
+  );
+});
+
+// ২. অ্যাক্টিভেট হওয়ার সময় পুরোনো v1, v2, v3 এর আবর্জনা ফাইল ডিলিট করে দেবে
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // শুধু ফাইল মুছবে, মেহেরিনের ডাটা থাকবে সুরক্ষিত
+          }
+        })
+      );
     })
   );
 });
